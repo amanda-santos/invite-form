8ca5a19c0cf75d51bb2ff5fcab467f36d7aeaa85
Amanda Santos <amandasf0200@gmail.com>
2025-02-23 18:32:05 -0300

    chore: 🔧 setup TailwindCSS and CSS variables

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="bg-shape-background h-screen">
+    <h1 class="text-3xl font-bold text-brand-light font-body">Hello world!</h1>
+    <h2 class="text-3xl font-heading text-accent-fuschia font-bold">
+      Hi, I'm a Nuxt 3 app
+    </h2>
+    <h3 class="text-3xl font-logo text-accent-purple">This is a test</h3>
   </div>
 </template>
